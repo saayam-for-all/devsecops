b4ab7e1887f6eb2e76c7362f94bcb64b743c9526
Uzair Syed <umssyed@gmail.com>
2025-09-06 03:03:40 -0600

    Cleaned up code for inactive users and console access

--- IAM_User_Cleanup_Automation_Issue#49.py
+++ IAM_User_Cleanup_Automation_Issue#49.py
@@ -1,86 +1,115 @@
 #!/usr/bin/python3
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 current_date = datetime.now()
 
-
-def get_users():
-    
+def get_inactive_users():
+    print("********************* GET_INACTIVE_USERS *********************")
     try:
-        iam_client=boto3.client('iam')
-        users_info= iam_client.list_users()
-        #print(users_info)
-        
-        #today = datetime.now()
+        # Conenct to IAM
+        iam_client = boto3.client('iam')
+        # Define inactivity cutoff time (100 days for example)
+        cutoff = datetime.now(timezone.utc) - timedelta(days=100)
+        # These are the inactive users to act upon
+        flagged_users = [] # This will be a list of dicts of inactive users, containing username, userID, reason
+        flagged_usernames = [] # This will be a list of inactive users and contain only their username for easy use
 
-        filtered_users = []
-        for user in users_info["Users"]:
-            username = user["UserName"]
-            user_id = user["UserId"]
-            password_last_used = user.get("PasswordLastUsed")  # can be None
 
-           #logic to check users who last logged in 100 days ago goes here. None for never logged in.(Currently using not None to check funtionality).
-            if password_last_used is None or password_last_used is not None: #or password_last_used <= today:
-                filtered_users.append({
-                    "UserName": username,
-                    "UserId": user_id,
-                    "PasswordLastUsed": password_last_used.date().strftime("%b %d %Y %H:%M:%S") if password_last_used else None
-                })
-        usernames=[]
-        for i in filtered_users:
-            usernames.append(i['UserName'])
-        print(usernames)
-        return usernames
+        # Use Pagination for > 100 users. list_users method only returns 100 users per call
+        paginator = iam_client.get_paginator('list_users')
+        # Paginate through all the pages of IAM users
+        for page in paginator.paginate():
+            # Loop through all the users in each "Page"
+            for user in page['Users']:
+                # Identify password last used
+                pw_last_used = user.get('PasswordLastUsed') # can be None
+
+                # Flag if password last used is NEVER or used more than 100 days ago (cutoff)
+                if pw_last_used is None or pw_last_used <= cutoff:
+                    # Append to the list of flagged users.
+                    flagged_users.append({
+                        "UserName": user['UserName'],
+                        "UserId": user["UserId"],
+                        "Reason": "NEVER_USED" if pw_last_used is None else "INACTIVE_100_DAYS"
+                    })
+                    flagged_usernames.append(user['UserName'])
+
+        # Print details
+        print(f"The following number of users have been flagged: {len(flagged_usernames)}")
+        print(f"----Details:")
+        count = 1
+        for user_details in flagged_users:
+            print(f"{count}. Username: {user_details['UserName']}, Reason for inactivity: {user_details['Reason']}")
+            count = count + 1
+        return flagged_usernames
     
     except NoCredentialsError:
-      print(" AWS credentials are not configured")
-      print("Attach IAM role to instance to fetch IAM information")
-      return None
+        print(">> AWS credentials are not configured")
+        print(">> Attach IAM role to instance to fetch IAM information")
+        return None
     
-      
     except ClientError as e:
-       if e.response['Error']['Code'] == 'AccessDenied':
-          print(" Error: Permission denied")
-          print("Need IAM permission -> iam:ListUsers")
-          return None
-       else:
-          print(f"AWS Error: {e.response['Error']['Message']}")
-          return None
+        if e.response['Error']['Code'] == 'AccessDenied':
+            print(">> Error: Permission denied")
+            print(">> Need IAM permission -> iam:ListUsers")
+            return None
+        else:
+            print(f">> AWS Error: {e.response['Error']['Message']}")
+            return None
+
     except Exception as e:
-      print(f" Unexpected error: {str(e)}")
-      return None  
-    
-  
+        print(f">> Unexpected error: {str(e)}")
+        return None  
     
 def get_user_with_console_access(users_100):
+    print("\n\n********************* GET_USERS_WITH_CONSOLE_ACCESS *********************")
+    # Conenct to IAM
     iam_client = boto3.client('iam')
-    console_user_info=[]
+    # List of users with console access (LoginProfile)
+    console_users = []
+    # List of users with NO console access
+    no_console_users = []
+
+    # Loop through the inactive users
     for user in users_100:
         
         try:
-            console_user_info.append(iam_client.get_login_profile(UserName=user)['LoginProfile']['UserName'])
-            print(f"User {user} has console access","\n")
-           
-            
+            # If get_login_profile method for the user exists, they have console access
+            iam_client.get_login_profile(UserName=user)
+            console_users.append(user)
+        
+        # If an error occurs, there is a possibility the user does not have console access
         except ClientError as e:
+            # If error is NoSuchEntity -> Login profile does not exist
+            # No Console Password
             if e.response['Error']['Code'] == 'NoSuchEntity':
-                print(f"User {user} has NO console access")
-            if e.response['Error']['Code'] == 'AccessDenied':
-               print(" Error: Permission denied")
-               print("Need IAM permission: iam:Get-login-Profile")
-               return None
-            else:
-                print(f"AWS Error: {e.response['Error']['Message']}")
-                return None
-    print(console_user_info,"\n\n")
-    
-    return console_user_info
+                no_console_users.append(user)
+                continue
+            
+            # If caller does not have the appropriate access
+            elif e.response['Error']['Code'] == 'AccessDenied':
+                print(f"sds")
+                print(">> Error: Permission denied. Need IAM Permission 'iam:Get-login-profile'")
+                continue
 
+            # If there is any other AWS exception
+            else:
+                print(f">> AWS Error for {user}: {e.response['Error']['Message']}")
+                continue
+
+        except Exception as e:
+            print(f">> Unexpected Excetion Error: {str(e)}")
+            continue  
+    
+    print(f"Users with console access: {console_users}")
+    print(f"Users with NO console access: {no_console_users}")
+    return console_users, no_console_users
 
 
 def get_user_access_keys(console_users):
+    print("\n\n********************* GET_USERS_ACCESS_KEYS *********************")
     iam_client = boto3.client("iam")
     user_access_key_info = {}
 
@@ -137,18 +166,13 @@ def get_access_key_last_used(AccessKey):
     
     
     
-            
-        
-    
-    
-    
-
 if __name__=="__main__":
-    users_last_logged_100days=get_users()
+
+    # Identify all users who have been inactive for 100 days
+    users_last_logged_100days = get_inactive_users()
     
-    console_users=get_user_with_console_access(users_last_logged_100days)
+    # Identify all users who have console access and those that do not
+    console_users, no_console_users = get_user_with_console_access(users_last_logged_100days)
     
+    # Identify all users who have programmatic access
     get_user_access_keys(console_users)
-    
-    
-    
